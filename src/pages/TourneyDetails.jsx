f20e89188c306dd18e06608b41d8bee6edb09af4
David Lazaro <dlazaro7@gmail.com>
2022-11-16 21:49:26 +0100

    Tourney Phase DOne

--- src/pages/TourneyDetails.jsx
+++ src/pages/TourneyDetails.jsx
@@ -1,168 +1,198 @@
 import { useParams, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import { addTeamToTourneyService, detailsTourneyService, sortTeamsToTourneyService } from "../services/tourney.services";
+import {
+  addTeamToTourneyService,
+  detailsTourneyService,
+  sortTeamsToTourneyService,
+} from "../services/tourney.services";
 import { useEffect, useState } from "react";
 
-
-
 function TourneyDetails() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { tourneyId } = useParams();
-  const [details, setDetails] = useState()
+  const [details, setDetails] = useState();
   // SAVE DISORDER LIST TO START THE TOURNAMENT
   // const [disorderList, setDisorderList] = useState()
   // console.log("disorderList[0]",disorderList[0].name)
-  const [isFetching, setIsFetching] = useState(true)
+  const [isFetching, setIsFetching] = useState(true);
   // const [isFetching2, setIsFetching2] = useState(false)
-  console.log("details", details)
+  console.log("details", details);
 
   // ! QUARTER STATES
   // * Quarter A
-  const [quarterA1, setQuarterA1] = useState("Empty")
-  const [scoreA1, setScoreA1] = useState(0)
+  const [quarterA1, setQuarterA1] = useState("Empty");
+  const [scoreQA1, setScoreQA1] = useState("0");
 
-  const [quarterA2, setQuarterA2] = useState("Empty")
-  const [scoreA2, setScoreA2] = useState(0)
+  const [quarterA2, setQuarterA2] = useState("Empty");
+  const [scoreQA2, setScoreQA2] = useState("0");
 
   // * Quarter B
-  const [quarterB1, setQuarterB1] = useState("Empty")
-  const [scoreB1, setScoreB1] = useState(0)
+  const [quarterB1, setQuarterB1] = useState("Empty");
+  const [scoreQB1, setScoreQB1] = useState("0");
 
-  const [quarterB2, setQuarterB2] = useState("Empty")
-  const [scoreB2, setScoreB2] = useState(0)
+  const [quarterB2, setQuarterB2] = useState("Empty");
+  const [scoreQB2, setScoreQB2] = useState("0");
   // * Quarter C
-  const [quarterC1, setQuarterC1] = useState("Empty")
-  const [scoreC1, setScoreC1] = useState(0)
+  const [quarterC1, setQuarterC1] = useState("Empty");
+  const [scoreQC1, setScoreQC1] = useState("0");
 
-  const [quarterC2, setQuarterC2] = useState("Empty")
-  const [scoreC2, setScoreC2] = useState(0)
+  const [quarterC2, setQuarterC2] = useState("Empty");
+  const [scoreQC2, setScoreQC2] = useState("0");
 
   // * Quarter D
-  const [quarterD1, setQuarterD1] = useState("Empty")
-  const [scoreD1, setScoreD1] = useState(0)
+  const [quarterD1, setQuarterD1] = useState("Empty");
+  const [scoreQD1, setScoreQD1] = useState("0");
 
-  const [quarterD2, setQuarterD2] = useState("Empty")
-  const [scoreD2, setScoreD2] = useState(0)
+  const [quarterD2, setQuarterD2] = useState("Empty");
+  const [scoreQD2, setScoreQD2] = useState("0");
 
+  // * Semi A
+  const [semiA1, setSemiA1] = useState("Empty");
+  const [scoreSA1, setScoreSA1] = useState("0");
 
+  const [semiA2, setSemiA2] = useState("Empty");
+  const [scoreSA2, setScoreSA2] = useState("0");
 
+  // * Semi B
+  const [semiB1, setSemiB1] = useState("Empty");
+  const [scoreSB1, setScoreSB1] = useState("0");
 
+  const [semiB2, setSemiB2] = useState("Empty");
+  const [scoreSB2, setScoreSB2] = useState("0");
 
+  // * Final
+  const [final1, setFinal1] = useState("Empty");
+  const [scoreF1, setScoreF1] = useState("0");
+
+  const [final2, setFinal2] = useState("Empty");
+  const [scoreF2, setScoreF2] = useState("0");
+
+  // * Winner
+  const [winner, setWinner] = useState("Empty");
 
   useEffect(() => {
-    getData()
-    
-  },[])
+    getData();
+  }, []);
 
- 
-  
   const getData = async () => {
     try {
-      const response = await detailsTourneyService(tourneyId)
-    
-   
+      const response = await detailsTourneyService(tourneyId);
 
-      const tourneyDetails = response.data
-      
-      setDetails(tourneyDetails)
+      const tourneyDetails = response.data;
+
+      setDetails(tourneyDetails);
       // EN DETAILS TIENEN TODA LA INFO DE LOS TIERS DEL TORNEO
       // NO HARIA FALTA ESTADOS PARA CADA UNO
-     
-       if (tourneyDetails.quarterA.length === 2) {
-        setQuarterA1(tourneyDetails.quarterA[0].name)
-        setQuarterA2(tourneyDetails.quarterA[1].name)
-      //  } else if (tourneyDetails.quarterB.length === 2) {
-        setQuarterB1(tourneyDetails.quarterB[0].name)
-        setQuarterB2(tourneyDetails.quarterB[1].name)
-      //  } else if (tourneyDetails.quarterC.length === 2) {
-        setQuarterC1(tourneyDetails.quarterC[0].name)
-        setQuarterC2(tourneyDetails.quarterC[1].name)
-      // } else if (tourneyDetails.quarterD.length === 2) {
-        setQuarterD1(tourneyDetails.quarterD[0].name)
-        setQuarterD2(tourneyDetails.quarterD[1].name)
+
+      // * setTeamNameUpdate
+      // Quarters
+      if (tourneyDetails.quarterA.length === 2) {
+        setQuarterA1(tourneyDetails.quarterA[0].name);
+        setQuarterA2(tourneyDetails.quarterA[1].name);
+        //  } else if (tourneyDetails.quarterB.length === 2) {
+        setQuarterB1(tourneyDetails.quarterB[0].name);
+        setQuarterB2(tourneyDetails.quarterB[1].name);
+        //  } else if (tourneyDetails.quarterC.length === 2) {
+        setQuarterC1(tourneyDetails.quarterC[0].name);
+        setQuarterC2(tourneyDetails.quarterC[1].name);
+        // } else if (tourneyDetails.quarterD.length === 2) {
+        setQuarterD1(tourneyDetails.quarterD[0].name);
+        setQuarterD2(tourneyDetails.quarterD[1].name);
       }
 
-      setIsFetching(false)
-    } catch (error) {
-      navigate("/error")
-    }
-  }
+      // semifinals
+      setSemiA1(tourneyDetails.semiA[0].name);
+      setSemiA2(tourneyDetails.semiA[1].name);
 
-  
-  
+      setSemiB1(tourneyDetails.semiB[0].name);
+      setSemiB2(tourneyDetails.semiB[1].name);
+      // final
+      setFinal1(tourneyDetails.final[0].name);
+      setFinal2(tourneyDetails.final[1].name);
+      // winner
+      setWinner(tourneyDetails.winner.name);
+
+      // * setScoreUpdate
+      // quarters
+      setScoreQA1(tourneyDetails.scoreQA1);
+      setScoreQA2(tourneyDetails.scoreQA2);
+      setScoreQB1(tourneyDetails.scoreQB1);
+      setScoreQB2(tourneyDetails.scoreQB2);
+      setScoreQC1(tourneyDetails.scoreQC1);
+      setScoreQC2(tourneyDetails.scoreQC2);
+      setScoreQD1(tourneyDetails.scoreQD1);
+      setScoreQD2(tourneyDetails.scoreQD2);
+      // semifinals
+      setScoreSA1(tourneyDetails.scoreSA1)
+      setScoreSA2(tourneyDetails.scoreSA2)
+
+      setScoreSB1(tourneyDetails.scoreSB1)
+      setScoreSB2(tourneyDetails.scoreSB2)
+      // finals
+      setScoreF1(tourneyDetails.scoreF1)
+      setScoreF2(tourneyDetails.scoreF2)
+
+      setIsFetching(false);
+    } catch (error) {
+      navigate("/error");
+    }
+  };
 
   const handleAddTeamToTourney = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      await addTeamToTourneyService(tourneyId)
+      await addTeamToTourneyService(tourneyId);
       // navigate(`/tourney/${tourneyId}/details`)
-      navigate(`/list/${tourneyId}/details`)
+      navigate(`/list/${tourneyId}/details`);
     } catch (error) {
-      navigate("/error")
-      
+      navigate("/error");
     }
-  }
+  };
 
   const handleStartSort = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-       
-        const response = await sortTeamsToTourneyService(tourneyId)
-        console.log("entrando en estart")
-        
-        // getData()
-        console.log("RESPONSE", response.data[0].name)
+      const response = await sortTeamsToTourneyService(tourneyId);
+      console.log("entrando en estart");
 
+      // getData()
+      console.log("RESPONSE", response.data[0].name);
 
+      // CUANDO HAGAN EL SORT, ACTUALIZAN LOS DETALLES
+      // KATA => TIENE UNA ESTRUCTURA DE DATA CON VARIOS ARRAYS
+      // ACTUALIZAR CADA ARRAY CON LOS VALORES ABAJO
+      setQuarterA1(response.data[0].name);
+      setQuarterA2(response.data[1].name);
+      setQuarterB1(response.data[2].name);
+      setQuarterB2(response.data[3].name);
+      setQuarterC1(response.data[4].name);
+      setQuarterC2(response.data[5].name);
+      setQuarterD1(response.data[6].name);
+      setQuarterD2(response.data[7].name);
 
-
-        // CUANDO HAGAN EL SORT, ACTUALIZAN LOS DETALLES
-        // KATA => TIENE UNA ESTRUCTURA DE DATA CON VARIOS ARRAYS
-        // ACTUALIZAR CADA ARRAY CON LOS VALORES ABAJO
-       setQuarterA1(response.data[0].name)
-       setQuarterA2(response.data[1].name)
-       setQuarterB1(response.data[2].name)
-       setQuarterB2(response.data[3].name)
-       setQuarterC1(response.data[4].name)
-       setQuarterC2(response.data[5].name)
-       setQuarterD1(response.data[6].name)
-       setQuarterD2(response.data[7].name)
-
-       
       //  setTimeout(()=>{
-       
+
       // },2000)
       // console.log(response.data)
       // setDisorderList(response.data)
-      
-      navigate(`/list/${tourneyId}/details`)
-    } catch (error) {
-      navigate("/error")
-    }
-    
-  }
-  
-  
-  console.log(details)
-  
 
-  
-  
-  if (isFetching === true) {
-    return (
-      <h2>...Loading</h2>
-      )
+      navigate(`/list/${tourneyId}/details`);
+    } catch (error) {
+      navigate("/error");
     }
-    
-     const handleEditLink = (e) => {
-      
-      navigate(`/list/${tourneyId}/details/edit`)
-     }
-    
+  };
+
+  console.log("TOURNEYdETAILS", details);
+
+  if (isFetching === true) {
+    return <h2>...Loading</h2>;
+  }
+
+  const handleEditLink = (e) => {
+    navigate(`/list/${tourneyId}/details/edit`);
+  };
 
   return (
-
     <div className="tourney-list">
       <h3>TourneyDetails</h3>
 
@@ -173,7 +203,8 @@ function TourneyDetails() {
           </p>
           <h1 id="headline">Tournament</h1>
           <p className="year">
-            <i className="fa fa-star"></i> TEMPORADA <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i> TEMPORADA{" "}
+            <i className="fa fa-star"></i>
           </p>
           <p>NOMBRE DEL TORNEO</p>
         </div>
@@ -189,21 +220,24 @@ function TourneyDetails() {
               </div>
               <ul className="matchup">
                 <li className="team team-top">
-                {quarterA1}<span className="score">{scoreA1}</span>
+                  {quarterA1}
+                  <span className="score">{scoreQA1}</span>
                 </li>
                 <li className="team team-bottom">
-                {quarterA2}<span className="score">{scoreA2}</span>
+                  {quarterA2}
+                  <span className="score">{scoreQA2}</span>
                 </li>
               </ul>
               <ul className="matchup">
                 <li className="team team-top">
-                {quarterB1}<span className="score">{scoreB1}</span>
+                  {quarterB1}
+                  <span className="score">{scoreQB1}</span>
                 </li>
                 <li className="team team-bottom">
-                {quarterB2}<span className="score">{scoreB2}</span>
+                  {quarterB2}
+                  <span className="score">{scoreQB2}</span>
                 </li>
               </ul>
-              
             </div>
             {/* <!-- END ROUND ONE --> */}
 
@@ -214,48 +248,49 @@ function TourneyDetails() {
               </div>
               <ul className="matchup">
                 <li className="team team-top">
-                  &nbsp;<span className="score">&nbsp;</span>
+                  {semiA1}
+                  <span className="score">{scoreSA1}</span>
                 </li>
                 <li className="team team-bottom">
-                  &nbsp;<span className="score">&nbsp;</span>
+                  {semiA2}
+                  <span className="score">{scoreSA2}</span>
                 </li>
               </ul>
-            
             </div>
-           
-
-        
           </div>
-           {/* <!-- END ROUND TWO --> */}
+          {/* <!-- END ROUND TWO --> */}
           {/*  END ROUND THREE   */}
           <div className="champion">
             <div className="semis-l">
               <div className="round-details">
-                west semifinals <br />
+                Final <br />
                 <span className="date">March 26-28</span>
               </div>
               <ul className="matchup championship">
                 <li className="team team-top">
-                  &nbsp;<span className="vote-count">&nbsp;</span>
+                  {final1}
+                  <span className="score">{scoreF1}</span>
                 </li>
                 <li className="team team-bottom">
-                  &nbsp;<span className="vote-count">&nbsp;</span>
+                  {final2}
+                  <span className="score">{scoreF2}</span>
                 </li>
               </ul>
             </div>
-            <div className="final">
+            <div className="semis-l">
+              {" "}
+              <span className="round-details">champion</span>
               <i className="fa fa-trophy"></i>
               <ul className="matchup championship">
                 <li className="team team-top">
-                  &nbsp;<span className="vote-count">&nbsp;</span> 
+                  {winner}
+                  <span className="vote-count"></span>
                 </li>
               </ul>
             </div>
- 
           </div>
 
           <div className="split split-two">
-        
             {/* END ROUND THREE  */}
 
             <div className="round round-two">
@@ -265,13 +300,14 @@ function TourneyDetails() {
               </div>
               <ul className="matchup">
                 <li className="team team-top">
-                  &nbsp;<span className="score">&nbsp;</span>
+                  {semiB1}
+                  <span className="score">{scoreSB1}</span>
                 </li>
                 <li className="team team-bottom">
-                  &nbsp;<span className="score">&nbsp;</span>
+                  {semiB2}
+                  <span className="score">{scoreSB2}</span>
                 </li>
               </ul>
-            
             </div>
 
             {/* END ROUND TWO  */}
@@ -279,25 +315,28 @@ function TourneyDetails() {
             <div className="round round-one current">
               <div className="round-details">
                 Round 1<br />
-              <span className="date">March 16</span>
+                <span className="date">March 16</span>
               </div>
               <ul className="matchup">
                 <li className="team team-top">
-                {quarterC1}<span className="score">{scoreC1}</span>
+                  {quarterC1}
+                  <span className="score">{scoreQC1}</span>
                 </li>
                 <li className="team team-bottom">
-                {quarterC2}<span className="score">{scoreC2}</span>
+                  {quarterC2}
+                  <span className="score">{scoreQC2}</span>
                 </li>
               </ul>
               <ul className="matchup">
                 <li className="team team-top">
-                {quarterD1}<span className="score">{scoreD1}</span>
+                  {quarterD1}
+                  <span className="score">{scoreQD1}</span>
                 </li>
                 <li className="team team-bottom">
-                {quarterD2}<span className="score">{scoreD2}</span>
+                  {quarterD2}
+                  <span className="score">{scoreQD2}</span>
                 </li>
               </ul>
-              
             </div>
           </div>
         </div>
@@ -319,21 +358,17 @@ function TourneyDetails() {
       <div>
         <h3>Team List</h3>
         {details.teams.map((eachTeam) => {
-          return(
-            <p key={eachTeam._id}>{eachTeam.name}</p>
-          )
+          return <p key={eachTeam._id}>{eachTeam.name}</p>;
         })}
       </div>
 
       <Button onClick={handleStartSort}>Start</Button>
       <Button>Edit</Button>
-    
+
       <Button onClick={handleAddTeamToTourney}>signup Team</Button>
       <Button onClick={handleEditLink}>Edit Tourney</Button>
     </div>
-    )
-  }
-
-
+  );
+}
 
 export default TourneyDetails;
