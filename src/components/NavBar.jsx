129e3d205ba2f425bfc99d95be5da661d0017507
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-12-31 12:21:18 +0100

    new nav-bar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,61 +1,47 @@
+// Context
 import { useContext } from "react";
-
 import { AuthContext } from "../context/auth.context.js";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { useNavigate } from "react-router-dom";
+// Navigation
+import { useNavigate, Link } from "react-router-dom";
 
 function NavBar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { authenticateUser, isLoggedIn } = useContext(AuthContext);
-  
+
   const handleLogout = () => {
     localStorage.removeItem("authToken");
     authenticateUser();
-    navigate("/")
+    navigate("/");
   };
 
   return (
-    <div >
- {isLoggedIn === true ? (
- <>
-<Navbar bg="dark" variant="dark">
-  <Container>
-    <Navbar.Brand href="#home">eS-Challenge</Navbar.Brand>
-    <Nav className="nav">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/profile">Profile</Nav.Link>
-      <Nav.Link href="/tourneys">Tourneys</Nav.Link>
-      <Nav.Link href="/teams">Teams</Nav.Link>
-      <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-    </Nav>
-  </Container>
-</Navbar>
-</>
- ):(
-<>
-<Navbar bg="dark" variant="dark">
-  <Container>
-    <Navbar.Brand href="#home">eS-Challenge</Navbar.Brand>
-    <Nav className="nav">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/teams">Teams</Nav.Link>
-      <Nav.Link href="/tourneys">Tourneys</Nav.Link>
-      <Nav.Link href="/signup">Signup</Nav.Link>
-      <Nav.Link href="/login">login</Nav.Link>
-    </Nav>
-  </Container>
-</Navbar>
-</>
-
- )}
- 
-</div>
+    <div className="nav-bar">
+      {isLoggedIn === true ? (
+        <div>
+          <Link to="/" id="brand">eS-Challenge</Link>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/tourneys">Tourneys</Link>
+            <Link to="/teams">Teams</Link>
+            <Link onClick={handleLogout}>Logout</Link>
+          </nav>
+        </div>
+      ) : (
+        <div>
+          <Link href="/" id="brand">eS-Challenge</Link>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/teams">Teams</Link>
+            <Link to="/tourneys">Tourneys</Link>
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">login</Link>
+          </nav>
+        </div>
+      )}
+    </div>
   );
 }
 
-
 export default NavBar;
-
