22ff82e301faa9266b4b242cc082aa767e05c151
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-01-09 14:08:44 +0100

    Team List jsx and css done

--- src/pages/navbar/TeamList.jsx
+++ src/pages/navbar/TeamList.jsx
@@ -54,28 +54,10 @@ function TeamList() {
   }
 
   return (
-    // <div>
-    //   <h3>TeamList</h3>
-    //   <ListGroup>
-
-    //     {teamList.map((eachTeam) => {
-    //       return (
-    //         <div key={eachTeam._id}>
-    //          <ListGroup.Item variant="dark"> <p>
-    //             <Link to={`/team/${eachTeam._id}/details`}>
-    //               {eachTeam.name}
-    //             </Link>
-    //           </p></ListGroup.Item>
-    //         </div>
-    //       );
-    //     })}
-
-    //   </ListGroup>
-    // </div>
-    <div id="teams-container">
+    <div>
       {/* <CardGroup> */}
-      <div style={{ display: "block", width: "25%", margin: "auto" }}>
-        <h5 style={{ color: "white" }}>Find your team and sign up</h5>
+      <div className="search-team">
+        <h5>Find your team and sign up</h5>
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="search"
@@ -89,33 +71,19 @@ function TeamList() {
           </Button>
         </InputGroup>
       </div>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "40px" }}>
+      <div className="teams-container">
         {teamListSearch.map((eachTeam) => {
           return (
-            <Card
-              key={eachTeam._id}
-              style={{ width: "12rem", marginBotton: "20px", opacity: "0.7" }}
-            >
-              <Card.Img variant="top" src={eachTeam.picture} />
-              <Card.Body>
-                <Card.Title>{eachTeam.name}</Card.Title>
-                <Card.Text>Members:</Card.Text>
-              </Card.Body>
-
-              <ListGroup className="list-group-flush">
-                {eachTeam.members.map((eachMember) => {
-                  return (
-                    <ListGroup.Item key={eachMember._id}>
-                      {eachTeam.nameTag} - {eachMember.username}
-                    </ListGroup.Item>
-                  );
-                })}
-              </ListGroup>
-              <Card.Body>
-                <Link to={`/team/${eachTeam._id}/details`}>Team Details</Link>
-                {/* <Card.Link href="#">Another Link</Card.Link> */}
-              </Card.Body>
-            </Card>
+            <Link to={`/team/${eachTeam._id}/details`}>
+              <div className="card-team" key={eachTeam._id}>
+                <div className="div-img">
+                  <Card.Img variant="top" src={eachTeam.picture} />
+                </div>
+                <div className="name">
+                  <h5>{eachTeam.name}</h5>
+                </div>
+              </div>
+            </Link>
           );
         })}
       </div>
