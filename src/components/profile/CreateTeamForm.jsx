0b6ebe1a20b50254b6223af1596892c13f110c22
David Lazaro <dlazaro7@gmail.com>
2023-01-12 06:24:44 +0100

    rebase infoProfile

--- src/components/profile/CreateTeamForm.jsx
+++ src/components/profile/CreateTeamForm.jsx
@@ -22,7 +22,6 @@ function CreateTeamForm(props) {
   const [errorMessage, setErrorMessage] = useState("");
   const [errorMessage2, setErrorMessage2] = useState("");
 
-
   const [isFetching, setIsFetching] = useState(true);
 
   const handleNameChange = (e) => setNameInput(e.target.value);
@@ -49,10 +48,10 @@ function CreateTeamForm(props) {
 
       setIsFetching(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       if (error.response && error.response.status === 400) {
         setErrorMessage(error.response.data.errorMessage);
-        setErrorMessage2(error.response.data.errorMessage2)
+        setErrorMessage2(error.response.data.errorMessage2);
       } else {
         navigate(error);
       }
@@ -81,72 +80,78 @@ function CreateTeamForm(props) {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>Create new Team</h1>
-      <hr />
-      <Form style={{ display: "flex" }}>
-        <fieldset>
-          <Form.Group className="mb-3">
-          <Form.Label htmlFor="Name">{((errorMessage2 !== "" && nameInput === "") || (errorMessage === "Name must have min 4 letters"))&& <span className="error-message">{errorMessage2}</span>}Name</Form.Label>
-            <Form.Control
-              value={nameInput}
-              onChange={handleNameChange}
-              id="disabledTextInput"
-              placeholder="Name of Team"
-            /> 
-            
-          </Form.Group>
-         
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="nameTagdescription">{((errorMessage2 !== "" && nameTagInput === "") || (errorMessage === "Name Tag must have max 3 characters")) && <span className="error-message">{errorMessage2}</span>}Name Tag:</Form.Label>
-            <Form.Control
-              value={nameTagInput}
-              onChange={handNameTagChange}
-              id="disabledTextInput"
-              placeholder="Your Tag"
-            />
-          </Form.Group>
-          
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="picture">Picture</Form.Label>
-            <Form.Control
-              onChange={handlePictureChange}
-              type="file"
-              name="picture"
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="joinPassword">{((errorMessage2 !== "" && joinPasswordInput === "") || (errorMessage === "Join Password must have min 4 characters")) && <span className="error-message">{errorMessage2}</span>}JoinPassword: </Form.Label>
-            <Form.Control
-              value={joinPasswordInput}
-              onChange={handleJoinPasswordChange}
-              type="text"
-              placeholder="Password for user to join"
-            />
-          </Form.Group>
-          
-          {isLoadingPicture === true && <p>...loading picture</p>}
+    <section id="create-team-container">
+      <div className="create-team-container">
+      <form>
+        <h3>Create Your Team</h3>
+        <div className="input-container">
+          <input
+            value={nameInput}
+            onChange={handleNameChange}
+            id="disabledTextInput"
+          />
+          <label className={nameInput && "filled"} htmlFor="name">
+            {((errorMessage2 !== "" && nameInput === "") ||
+              errorMessage === "Name must have min 4 letters") && (
+              <span className="error-message">{errorMessage2}</span>
+            )}
+            Name
+          </label>
+        </div>
 
-          {pictureURL !== "" ? (
-            <img src={pictureURL} alt="pict" width={200} />
-          ) : (
-            <p>Choose image</p>
-          )}
+        <div className="input-container">
+          <input
+            value={nameTagInput}
+            onChange={handNameTagChange}
+            id="disabledTextInput"
+          />
+          <label className={nameTagInput && "filled"} htmlFor="nameTag">
+            {((errorMessage2 !== "" && nameTagInput === "") ||
+              errorMessage === "Name Tag must have max 3 characters") && (
+              <span className="error-message">{errorMessage2}</span>
+            )}
+            Name Tag
+          </label>
+        </div>
 
-          <Button type="submit" onClick={handleSubmit}>
-            Create your Team
-          </Button>
-          {errorMessage !== "" && <p className="error-message">{errorMessage}</p>}
-        </fieldset>
-      </Form>
-    </div>
+        
+
+        <div className="input-container">
+          <input
+            value={joinPasswordInput}
+            onChange={handleJoinPasswordChange}
+            type="text"
+          />
+          <label
+            className={joinPasswordInput && "filled"}
+            htmlFor="joinPassword"
+          >
+            {((errorMessage2 !== "" && joinPasswordInput === "") ||
+              errorMessage === "Join Password must have min 4 characters") && (
+              <span className="error-message">{errorMessage2}</span>
+            )}
+            Password for Join{" "}
+          </label>
+        </div>
+        <div className="uploader-pic">
+          <input onChange={handlePictureChange} type="file" name="picture" />
+          <label htmlFor="picture">Team Picture</label>
+        </div>
+        {isLoadingPicture === true && <p>...loading picture</p>}
+
+        {pictureURL !== "" ? (
+          <img src={pictureURL} alt="yourPic" width={200} className="uploader-img"  />
+        ) : (
+          <p> [ No Picture Selected ]</p>
+        )}
+
+        <button type="submit" onClick={handleSubmit}>
+          Accept
+        </button>
+        {errorMessage !== "" && <p className="error-message">{errorMessage}</p>}
+      </form>
+      </div>
+    </section>
   );
 }
 
