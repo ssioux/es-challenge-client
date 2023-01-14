afdc06b255dda20ba2448d1e04ec55bebc8be484
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-01-14 13:49:34 +0100

    Instructions component added, hashlink imported

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Instructions() {
+  return (
+    <div className='Instructions-container'>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+        <h2>Instructions</h2>
+    </div>
+  )
+}
+
+export default Instructions
--- src/pages/navbar/Home.jsx
+++ src/pages/navbar/Home.jsx
@@ -2,26 +2,61 @@
 // Components
 // import Header from "../../components/home/Header";
 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Instructions from "../../components/Instructions";
+import { HashLink } from 'react-router-hash-link';
+
 function Home() {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
   return (
     <section className="home">
       {/* <Header /> */}
-   
 
-      <div className="home-braket-pic">
+      {/* <div className="home-braket-pic">
 <img src="../../../images/braket-show.png" alt="show-braket-home" />
 
+      </div> */}
+
+      <div className="home-paraph">
+        <p id="title-access">
+          {" "}
+          <span>Login Access </span>
+        </p>
+        <p>Fell free as an admin using the user bellow:</p>
+        <p>
+          <span>E-mail: </span>user1@user1.com
+        </p>
+        <p>
+          {" "}
+          <span>Password:</span> Banana99!
+        </p>
       </div>
 
-         <div className="home-paraph">
-   
-       
-        <p id="title-access">  <span>Login Access </span></p>
-        <p><span>E-mail: </span>user1@user1.com</p>
-        <p> <span>Password:</span> Banana99!</p>
+      <div className="home-title">
+        <div>
+          <h1>Wellcome to esChallenge</h1>
+          <h4>Join us and create your favourite game's tourney</h4>
+        </div>
+        <div className="home-button-instructions">
+          <HashLink
+            smooth to ="/#Instructions"
+           
+          >
+            <button>Instructions</button>
+          </HashLink>
+        </div>
       </div>
-
-
+      <div className="home-image-tourney">
+        <img src="../../../images/tourney-screen-shot.png" alt="screen-shot" />
+      </div>
+      <div id="Instructions">
+        <Instructions />
+      </div>
     </section>
   );
 }
