88846977a786622379bcf176b82563c2dea5899a
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-01-14 19:33:24 +0100

    home page, instructions done

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,19 +1,78 @@
-import React from 'react'
+import React from "react";
 
 function Instructions() {
   return (
-    <div className='Instructions-container'>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
-        <h2>Instructions</h2>
+    <div className="instructions-container">
+      <div>
+        <h1>
+          Follow the instructions bellow to become a <span>Tourney Master</span>
+        </h1>
+      </div>
+
+      <div className="instructions-card">
+        <div className="instructions-card-img">
+          <img src="../../images/create-team-ss.png" alt="create-team" />
+        </div>
+        <div className="instructions-card-text">
+          <h3>Create your Team</h3>
+          <ul>
+            <li>First of all, create your team to can access to tourney.</li>
+            <li>
+              Write a password and don't forget it, because your team members
+              must write it to can join them to your team.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="instructions-card">
+        <div className="instructions-card-text">
+          <h3>Search your friends teams and join</h3>
+          <ul>
+            <li>Select the team which you want to register.</li>
+            <li>Introduce the password and be ready for the tourney.</li>
+          </ul>
+        </div>
+        <div className="instructions-card-img">
+          <img
+            src="../../images/team-toadd-screen-shot.png"
+            alt="create-team"
+          />
+        </div>
+      </div>
+      <div className="instructions-card">
+        <div className="instructions-card-img">
+          <img src="../../images/create-game-ss.png" alt="create-team" />
+        </div>
+        <div className="instructions-card-text">
+          <h3>Game</h3>
+          <ul>
+            <li>Think about your favourite game and choose a picture.</li>
+            <li>
+              Fill the form. Remind the name, you will use it in the next step.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1>
+          🚀 Now, you are ready to create your <span>Own Tourney</span>
+        </h1>
+      </div>
+      <div className="instructions-card">
+        <div className="instructions-card-text">
+          <h3>Tournament</h3>
+          <ul>
+            <li>Fill the form.</li>
+            <li>Wait until the eight teams will be registered.</li>
+            <li>Button Start will be enabled, press and edit the Tourney.</li>
+          </ul>
+        </div>
+        <div className="instructions-card-img">
+          <img src="../../images/create-tourney-ss.png" alt="create-team" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Instructions
+export default Instructions;
