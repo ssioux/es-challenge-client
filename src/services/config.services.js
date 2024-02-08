c010855118cfe565309371d545b5bd518faabef4
David Lazaro <dlazaro7@gmail.com>
2024-02-08 11:00:39 +0100

    d

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL
+    baseURL: "https://worried-loincloth-seal.cyclic.app/api"
 });
 
 // Find Token and includes in.
