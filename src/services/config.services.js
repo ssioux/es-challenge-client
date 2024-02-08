f63248cdc1ed73b69227ebef65d9aa68e749d4dc
David Lazaro <dlazaro7@gmail.com>
2024-02-08 10:36:23 +0100

    cfg axios fixed

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-    baseURL: process.env.REACT_APP_SERVER
+    baseURL: process.env.REACT_APP_SERVER_URL
 });
 
 // Find Token and includes in.
